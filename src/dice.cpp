6243fec8136bd9e88dd9b1cf72d7948465a5af20
Júlia Guilhermino <juh.guilhermino03@gmail.com>
2023-09-05 17:37:06 -0300

    dice ok

--- src/dice.cpp
+++ src/dice.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <experimental/random>
 using namespace std;
 
 enum players_e {
@@ -23,19 +24,21 @@ class Dice {
     public:
         //método que escolhe aleatóriamente jogador que vai começar o jogo;
         int choose_player() {
-            
-            return;
+            int player = experimental::randint(0,1);
+            return player;
         };
 
         //resgistra quem está na jogada
-        int update_current_player() {
+        /*int update_current_player() {
+            
+            return 0;
+        }*/
 
-            return;
-        }
+        //sortear ação da máquina
 
         //rolar o dado
         int roll_dice(){
-
-            return;
+            int dice = experimental::randint(1,6);
+            return dice;
         }
 };
