0c17cb7be2d65c1b5107999daa7fb3887d05f71d
Júlia Guilhermino <juh.guilhermino03@gmail.com>
2023-09-05 20:23:31 -0300

    geral update

--- src/dice.cpp
+++ src/dice.cpp
@@ -17,7 +17,7 @@ enum players_e {
 
 class Dice {
     private:
-        int current_player;
+        int next_player;
         int dice; //num sorteado no dado
         int dice_faces[6] = {1, 2, 3, 4, 5, 6};
 
@@ -25,16 +25,20 @@ class Dice {
         //método que escolhe aleatóriamente jogador que vai começar o jogo;
         int choose_player() {
             int player = experimental::randint(0,1);
-            return player;
+            return players_e(player);
         };
 
         //resgistra quem está na jogada
-        /*int update_current_player() {
+        int update_player(int current_player) {
+            if (current_player == 1){
+                next_player = players_e(current_player-1);
+            } 
+            if (current_player == 0){
+                next_player = players_e(current_player+1);
+            }
             
-            return 0;
-        }*/
-
-        //sortear ação da máquina
+            return next_player;
+        }
 
         //rolar o dado
         int roll_dice(){
--- src/game_controller.cpp
+++ src/game_controller.cpp
@@ -12,7 +12,6 @@ using namespace std;
 
 class Game_Controller {
     private:
-        Player* current_player;
         //representam as ações do jogo
         enum game_actions {
             HOLD=0,
@@ -39,10 +38,16 @@ class Game_Controller {
         Player p_human; 
         Player p_machine;
 
+        //criando uma instacia da classe Dice
+        Dice dice;
+
         //variaveis que controlam o estado do jogo
+        int jogador_rodada;
         bool asked_to_quite;
         bool end_game = false;
         int game_state;
+        int starting_player;
+        int current_player;
 
 
     public:
@@ -54,10 +59,7 @@ class Game_Controller {
 
             //usando a classe dice pra escolher o jogador inicial
             Dice dice;
-            int starting_player = dice.choose_player();
-
-            //definir o jogador atual com base na escolha aleatória
-            current_player = (starting_player == HUMAN) ? &p_human : &p_machine;
+            starting_player = dice.choose_player();
 
             //solicitar o nome do jogador humano
             cout << "Informe seu nome: ";
@@ -66,53 +68,6 @@ class Game_Controller {
             //definir o estado inicial do jogo
             game_state = game_state_e::STARTING;
 
-        };
-
-        /// Renders the game to the user.
-        void render(void) const{
-            if ( game_state == game_state_e::STARTING ){
-                // Do nothing in this state.
-            } else if ( game_state == game_state_e::WELCOME){
-                // Welcome msg and rules.
-            } else if ( game_state == game_state_e::PLAYING ){
-
-            } else if ( game_state == game_state_e::ROLLING){
-
-            } else if ( game_state == game_state_e::HOLDING ){
-
-            } else if ( game_state == game_state_e::UPDATING_SCORE ){
-
-            } else if ( game_state == game_state_e::ENDING ){
-
-            } else if ( game_state == game_state_e::QUITTING ){
-
-            }
-        };
-        
-        /// Update the game based on the current game state.
-        void update(void){
-            if (game_state == game_state_e::STARTING ){
-                // Registrar no Dice de quem começa
-
-                //Go to WELCOME state
-                game_state = game_state_e::WELCOME;
-            } else if ( game_state == game_state_e::WELCOME ){
-                // Go to PLAY state.
-                game_state = game_state_e::PLAYING;
-            } else if ( game_state == game_state_e::PLAYING ){
-
-            } else if ( game_state == game_state_e::ROLLING ){
-
-            } else if ( game_state == game_state_e::HOLDING ){
-
-            } else if ( game_state == game_state_e::UPDATING_SCORE ){
-
-            } else if ( game_state == game_state_e::QUITTING ){
-
-            } else {
-                // Do nothing in these states.
-                //  + ENDING
-            }
         };
 
         /// Process user input events, depending on the current game state.
@@ -128,7 +83,11 @@ class Game_Controller {
 
             }
             else if ( game_state == game_state_e::PLAYING ){
-                cout << ">>> The current player is: " << current_player->get_name() << endl;
+                cout << ">>> The current player is: " << p_human.get_name() << endl;
+
+                //aqui tem que colocar um if
+                //se for a maquina tem que chamar a funçãod e escolha da operação
+                //se for o jogador humano tem essa mensagem 
                 cout << "Commands syntax:" << endl;
                 cout << " <Enter>      -> ROLL the dice." << endl;
                 cout << " 'r' + <Enter> -> ROLL the dice." << endl;
@@ -159,6 +118,55 @@ class Game_Controller {
 
             }
         };
+
+        /// Update the game based on the current game state.
+        void update(void){
+            if (game_state == game_state_e::STARTING ){
+                //definir o jogador atual com base na escolha aleatória
+                current_player = starting_player;
+
+                //Go to WELCOME state
+                game_state = game_state_e::WELCOME;
+            } else if ( game_state == game_state_e::WELCOME ){
+                // Go to PLAY state.
+                game_state = game_state_e::PLAYING;
+            } else if ( game_state == game_state_e::PLAYING ){
+
+            } else if ( game_state == game_state_e::ROLLING ){
+
+            } else if ( game_state == game_state_e::HOLDING ){
+
+            } else if ( game_state == game_state_e::UPDATING_SCORE ){
+
+            } else if ( game_state == game_state_e::QUITTING ){
+
+            } else {
+                // Do nothing in these states.
+                //  + ENDING
+            }
+        };
+
+        /// Renders the game to the user.
+        void render(void) const{
+            if ( game_state == game_state_e::STARTING ){
+                // Do nothing in this state.
+            } else if ( game_state == game_state_e::WELCOME){
+                // Welcome msg and rules.
+            } else if ( game_state == game_state_e::PLAYING ){
+
+            } else if ( game_state == game_state_e::ROLLING){
+
+            } else if ( game_state == game_state_e::HOLDING ){
+
+            } else if ( game_state == game_state_e::UPDATING_SCORE ){
+
+            } else if ( game_state == game_state_e::ENDING ){
+
+            } else if ( game_state == game_state_e::QUITTING ){
+
+            }
+        };
+
         /// Returns true when the user wants to quit the game.
         bool game_over(void){
             end_game = true;
