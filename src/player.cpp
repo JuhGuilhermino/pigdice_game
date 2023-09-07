758bb4430797ac2f0af7508b636c5e94612bdcf3
Júlia Guilhermino <juh.guilhermino03@gmail.com>
2023-09-07 16:49:12 -0300

    player update

--- src/player.cpp
+++ src/player.cpp
@@ -13,10 +13,13 @@ struct history{
 
 class Player {
     private:
-        string player_name = "Machine"; //nome do jogador
-        int turn_score = 0; //pontuação da rodada atual
-        int score = 0;
-        vector <history> tunrs_history;
+        string player_name = "Machine";  // nome do jogador
+        int turn_score = 0;              // pontuação do turno atual
+        int turn_rolls = 0;              // quantidade de jogadas no truno
+        int score = 0;                   // pontuação geral
+        vector <history> tunrs_history;  // historico de pontos e numero de jogadas por tundo
+        int last_dice;                   // numero que saiu na jogada anterior
+        int current_dice;                // numero que saiu na jogada atual
     
     public:
         //obtém o nome do jogador
@@ -30,24 +33,36 @@ class Player {
             return player_name;
         }
 
-        //adiciona o total de rolls e pontos obitidos na rodada
+        //obtém a pontuação da rodada atual e conta o numero de jogadas
         void set_turn_score(int points){
-            turn_score += points;
+            turn_score += points; 
+            turn_rolls += 1;    
         };
 
-        //obtém a pontuação da rodada atual
+        //informa os pontos do turno
         int get_turn_score() const{
             return turn_score;
         }
 
-        //adiciona o numero de pontos da rodada no total de pontos do jogo
+        //adiciona o numero de pontos da rodada no total de pontos do jogo e atualiza histórico
         void update_score (){
             score += turn_score;
+            tunrs_history.push_back({turn_rolls, turn_score});
             turn_score = 0; //zera a pontuação da rodada atual
+            turn_rolls = 0;
         };
 
-        //obtém a pontuação total do jogador
+        //informa a pontuação total do jogador
         int get_score() const{
             return score;
-        }
+        };
+
+        //verificar se o jogador tirou dois npuemros consecutivos
+        bool check_dices(int points) {
+            last_dice = current_dice;
+            current_dice = points;
+            if (last_dice == current_dice) {
+                return true;
+            }  
+        };
 };
